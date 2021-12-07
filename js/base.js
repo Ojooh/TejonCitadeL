a1d46c2bd8e0b8fe020641b3a6e561ed5798d703
Ojooh <davidmatthew708@gmail.com>
2021-12-07 17:24:00 +0100

    done with animation buug and all other components

--- js/base.js
+++ js/base.js
@@ -1,15 +1,23 @@
 jQuery(window).ready(function ($) {
+    var preloader = $("#preloader-active");
     var header = $("#header");
     var about = $("#aboutUs");
     var videBG = $("#video-bg");
     var btcCounter = $("#btc-counter");
     var newsletter = $("#newsletter");
-    var footer = $("footer");
+    var footer = $("#footer");
 
+    preloader.load('components/preloader.html');
     header.load('components/header.html');
     about.load('components/aboutUs.html');
     videBG.load('components/videoBg.html');
     btcCounter.load('components/BTCCounter.html');
     newsletter.load('components/newsletter.html');
-    // footer.load('components/footer.html');
+    footer.load('components/footer.html');
+
+    $('#preloader-active').delay(4500).fadeOut('slow');
+    $('body').delay(4500).css({ 'overflow': 'visible' });
+    var wow = new WOW({ offset: 100, mobile: false });
+    wow.init();
+
 });
